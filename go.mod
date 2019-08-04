a513663a7254efa51efda567506c357b1e6c534d
Qian Wang <cbeuw.andy@gmail.com>
2019-08-04 17:16:18 +0100

    Use a fork of ratelimit for unlimited bucket

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/cbeuw/Cloak
 
+go 1.12
+
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/cbeuw/ratelimit v1.1.0
 	github.com/gorilla/mux v1.7.3
-	github.com/juju/ratelimit v1.0.1
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	golang.org/x/crypto v0.0.0-20190123085648-057139ce5d2b
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- internal/multiplex/qos.go
+++ internal/multiplex/qos.go
@@ -3,7 +3,7 @@ package multiplex
 import (
 	"sync/atomic"
 
-	"github.com/juju/ratelimit"
+	"github.com/cbeuw/ratelimit"
 )
 
 // Valve needs to be universal, across all sessions that belong to a user
@@ -14,8 +14,8 @@ type Valve struct {
 	// rx is from client to server, tx is from server to client
 	// DO NOT use terms up or down as this is used in usermanager
 	// for bandwidth limiting
-	rxtb atomic.Value // *ratelimit.Bucket
-	txtb atomic.Value // *ratelimit.Bucket
+	rxtb ratelimit.Bucket
+	txtb ratelimit.Bucket
 
 	rx *int64
 	tx *int64
@@ -24,24 +24,33 @@ type Valve struct {
 func MakeValve(rxRate, txRate int64) *Valve {
 	var rx, tx int64
 	v := &Valve{
-		rx: &rx,
-		tx: &tx,
+		rxtb: ratelimit.NewLimitedBucketWithRate(float64(rxRate), rxRate),
+		txtb: ratelimit.NewLimitedBucketWithRate(float64(txRate), txRate),
+		rx:   &rx,
+		tx:   &tx,
 	}
-	v.SetRxRate(rxRate)
-	v.SetTxRate(txRate)
 	return v
 }
 
-var UNLIMITED_VALVE = MakeValve(1<<63-1, 1<<63-1)
+func MakeUnlimitedValve() *Valve {
+	var rx, tx int64
+	v := &Valve{
+		rxtb: ratelimit.NewUnlimitedBucket(),
+		txtb: ratelimit.NewUnlimitedBucket(),
+		rx:   &rx,
+		tx:   &tx,
+	}
+	return v
+}
 
-func (v *Valve) SetRxRate(rate int64) { v.rxtb.Store(ratelimit.NewBucketWithRate(float64(rate), rate)) }
-func (v *Valve) SetTxRate(rate int64) { v.txtb.Store(ratelimit.NewBucketWithRate(float64(rate), rate)) }
-func (v *Valve) rxWait(n int)         { v.rxtb.Load().(*ratelimit.Bucket).Wait(int64(n)) }
-func (v *Valve) txWait(n int)         { v.txtb.Load().(*ratelimit.Bucket).Wait(int64(n)) }
-func (v *Valve) AddRx(n int64)        { atomic.AddInt64(v.rx, n) }
-func (v *Valve) AddTx(n int64)        { atomic.AddInt64(v.tx, n) }
-func (v *Valve) GetRx() int64         { return atomic.LoadInt64(v.rx) }
-func (v *Valve) GetTx() int64         { return atomic.LoadInt64(v.tx) }
+var UNLIMITED_VALVE = MakeUnlimitedValve()
+
+func (v *Valve) rxWait(n int)  { v.rxtb.Wait(int64(n)) }
+func (v *Valve) txWait(n int)  { v.txtb.Wait(int64(n)) }
+func (v *Valve) AddRx(n int64) { atomic.AddInt64(v.rx, n) }
+func (v *Valve) AddTx(n int64) { atomic.AddInt64(v.tx, n) }
+func (v *Valve) GetRx() int64  { return atomic.LoadInt64(v.rx) }
+func (v *Valve) GetTx() int64  { return atomic.LoadInt64(v.tx) }
 func (v *Valve) Nullify() (int64, int64) {
 	rx := atomic.SwapInt64(v.rx, 0)
 	tx := atomic.SwapInt64(v.tx, 0)
