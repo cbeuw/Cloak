140c8022f5be21f7e1ef7e259db2cfa295b10be7
Andy Wang <cbeuw.andy@gmail.com>
2020-04-09 00:37:59 +0100

    Unbreak server keygen

--- cmd/ck-server/keygen.go
+++ cmd/ck-server/keygen.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"github.com/cbeuw/Cloak/internal/ecdh"
 	"github.com/cbeuw/Cloak/internal/util"
 )
@@ -9,12 +10,12 @@ import (
 func generateUID() string {
 	UID := make([]byte, 16)
 	util.CryptoRandRead(UID)
-	return b64(UID)
+	return base64.StdEncoding.EncodeToString(UID)
 }
 
 func generateKeyPair() (string, string) {
 	staticPv, staticPub, _ := ecdh.GenerateKey(rand.Reader)
 	marshPub := ecdh.Marshal(staticPub)
 	marshPv := staticPv.(*[32]byte)[:]
-	return b64(marshPub), b64(marshPv)
+	return base64.StdEncoding.EncodeToString(marshPub), base64.StdEncoding.EncodeToString(marshPv)
 }
